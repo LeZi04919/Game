fed0ab65abdc156485633209359c5cc165c66181
LeZi <bb20048862@gmail.com>
2023-11-13 18:30:46 +0800

    Add: some Events

--- Functions.cs
+++ Functions.cs
@@ -1,9 +1,16 @@
-﻿using System;
+﻿using RoguelikeGame.Prefabs;
+using System;
 
 namespace RoguelikeGame
 {
     internal partial class Game
     {
-
+        static PrefabCollection Prefabs = new();//当前场景下的实体集合
+        static event Action<Prefab> PrefabKilledEvent = prefab => 
+        {
+            if(prefab.Type is PrefabType.Monster)
+                Console.WriteLine("");
+        };//被杀死对象
+        static event Action<Prefab,Prefab,long> PrefabAttacked;//Source(攻击者)，Target(受击者),伤害大小
     }
 }
