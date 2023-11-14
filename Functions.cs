999002f34e2b5a7d0cc64094eb198e9badc7ccbb
LeZi <bb20048862@gmail.com>
2023-11-14 20:35:11 +0800

    Add: 加权随机实现

--- Functions.cs
+++ Functions.cs
@@ -1,5 +1,7 @@
 ﻿using RoguelikeGame.Prefabs;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RoguelikeGame
 {
@@ -15,8 +17,26 @@ namespace RoguelikeGame
 
         public static void CreateMonsters(MonsterType maxLevel)
         {
-
+            
         }
-
+        public static T WeightedRandom<T>(IDictionary<T, double> pairs) where T : notnull
+        {
+            double totalWeight = pairs.Values.Sum();
+            var randomDouble = new Random().NextDouble();
+            var randomResult = totalWeight * randomDouble;
+            foreach (var pair in pairs)
+                if ((randomResult -= pair.Value) < 0)
+                    return pair.Key;
+            return pairs.Keys.Last();
+        }//加权随机
+        public static T WeightedRandom<T>(IList<T> Keys, double[] weights) where T : notnull
+        {
+            double totalWeight = weights.Sum();
+            var randomResult = new Random().NextDouble() * totalWeight;
+            foreach (var weight in weights)
+                if ((randomResult -= weight) < 0)
+                    return Keys[Array.IndexOf(weights,weight)];
+            return Keys.Last();
+        }//加权随机
     }
 }
